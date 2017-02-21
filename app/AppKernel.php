69274b749658791d91c8b6eb75e00de860c941d2
Clément Long <clmt.long@gmail.com>
2017-02-21 22:28:52 +0100

    UserBundle : Init

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new CatBundle\CatBundle(),
             new Vich\UploaderBundle\VichUploaderBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
